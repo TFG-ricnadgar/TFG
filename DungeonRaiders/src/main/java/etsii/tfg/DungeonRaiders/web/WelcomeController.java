f6e20c2b2b2afe5a86d05cc85c2822a4d61b076b
Rick3600 <nadalrick@gmail.com>
2023-01-09 19:28:49 +0100

    Pagina de bienvenida

--- DungeonRaiders/src/main/java/etsii/tfg/DungeonRaiders/web/WelcomeController.java
+++ DungeonRaiders/src/main/java/etsii/tfg/DungeonRaiders/web/WelcomeController.java
@@ -1,13 +1,16 @@
 package etsii.tfg.DungeonRaiders.web;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WelcomeController {
-    
-    @GetMapping("/")
-    public String index() {
-        return "This is a Dungeon Raider welcome page";
+
+    @GetMapping(value = { "/", "/inicio" })
+    public String index(Map<String, Object> model) {
+        return "welcome";
     }
 }
